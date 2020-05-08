f0b9dcb59c2f368bd170efbde5cf8407ee13c134
Kara-Mel <64920362+Kara-Mel@users.noreply.github.com>
2020-05-08 16:30:43 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$clans = json_decode(file_get_contents('http://bv.mrdbx.ru/services/api_proxy.php?content=clans&callback.json'),true);
+$clans = json_decode(file_get_contents('https://berserktcg.ru/api/export/clans.json'),true);
 require("clans.php");
 
 ?>
+
